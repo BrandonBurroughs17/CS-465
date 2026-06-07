49cd36825fdaf040c479fd82e28939697c43d833
Brandon Burroughs <brandon.becker4115@gmail.com>
2026-06-07 15:00:23 -0600

    Completed Module 5 REST API implementation

--- travlr/app.js
+++ travlr/app.js
@@ -9,7 +9,7 @@ require('./app_server/models/db');
 const hbs = require('express-handlebars');
 
 const indexRouter = require('./routes/index');
-const apiRouter = require('./app_server/routes/index');
+const apiRouter = require('./app_api/routes/index');
 
 const app = express();
 
@@ -34,6 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', apiRouter);
 
 
 app.use(function(req, res, next) {
